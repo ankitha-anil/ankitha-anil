8e354732be406d6c70115962704b6bb28eeda31b
ankithaanil <ankitha001@e.ntu.edu.sg>
2021-02-02 00:10:51 +0800

    Updated skills section

--- script.js
+++ script.js
@@ -13,6 +13,42 @@ function sleep(ms) {
     );
   }
 
+  function overlay(){
+    document.getElementsByClassName("overlay").style.opacity= "1";
+}
+
+ function showLanguage(){
+    document.getElementById("language").style.visibility= "visible";
+    document.getElementById("language-2").style.visibility= "visible";
+   // document.getElementById("tools").style.visibility= "hidden";
+   // document.getElementById("learning").style.visibility= "hidden";
+    document.getElementsByClassName("skills-there")[0].style.transition="0.3s";
+    document.getElementsByClassName("skills-there")[0].style.height="500px";
+    document.getElementsByClassName("slider")[0].style.overflow="none";
+    document.getElementsByClassName("skills-tab")[0].style.height="70px";
+    
+ }
+
+ function showTools(){
+    document.getElementById("tools").style.visibility= "visible";
+    //document.getElementById("language").style.visibility= "hidden";
+    //document.getElementById("language-2").style.visibility= "hidden";
+    //document.getElementById("learning").style.visibility= "hidden";
+    document.getElementsByClassName("skills-there")[0].style.transition="0.3s";
+    document.getElementsByClassName("skills-there")[0].style.height="300px";
+    document.getElementsByClassName("skills-tab")[0].style.height="130px";
+ }
+
+ function showLearning(){
+    document.getElementById("learning").style.visibility= "visible";
+   // document.getElementById("language").style.visibility= "hidden";
+    //document.getElementById("language-2").style.visibility= "hidden";
+    //document.getElementById("tools").style.visibility= "hidden";
+    document.getElementsByClassName("skills-there")[0].style.transition="0.3s";
+    document.getElementsByClassName("skills-there")[0].style.height="300px";
+    document.getElementsByClassName("skills-tab")[0].style.height="130px";
+ }
+
 async function changeText(){
     const words=["Design","Development","Art","Travel"];
     let i=1;
